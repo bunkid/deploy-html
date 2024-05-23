e41e0e908edbed428f195a668476d21a6f92015f
bunkid <102501572+bunkid@users.noreply.github.com>
2024-05-23 13:40:59 +0000

    day  la file user hen

--- src/dev.js
+++ src/dev.js
@@ -6,28 +6,7 @@ const port = 3000
 app.use(express.urlencoded());
 app.use(express.json())
 
-const users = [
-    {
-      id: 1,
-      name: "Toi la nguoi 1",
-      Age: 18
-    },
-    {
-      id: 2,
-      name: "Toi la nguoi 2",
-      Age: 19
-    },
-    {
-      id: 3,
-      name: "Toi la nguoi 3",
-      Age: 20
-    },
-    {
-      id: 4,
-      name: "Toi la nguoi 4",
-      Age: 21
-    }
-  ]
+
 
   const logToken = (req, res, next) => {
     req.user= 'ten'
